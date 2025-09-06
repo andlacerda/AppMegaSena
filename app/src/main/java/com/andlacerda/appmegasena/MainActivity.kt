5580ec9196c90d0ea9ca1e6d4c04c96845e12277
Anderson <andersonlacerdadev@gmail.com>
2025-09-06 19:15:49 -0300

    Implemented the SharedPreferences to the last bet.

--- app/src/main/java/com/andlacerda/appmegasena/MainActivity.kt
+++ app/src/main/java/com/andlacerda/appmegasena/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.andlacerda.appmegasena
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -14,19 +16,30 @@ import android.widget.Toast
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val editText: EditText = findViewById(R.id.edit_number);
+        val editText: EditText = findViewById(R.id.edit_number)
         val txtResult: TextView = findViewById(R.id.txt_result)
         val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val result = prefs.getString("result", null)
+        if (result != null) {
+            txtResult.text = "Ultima aposta: $result"
+        }
 
         btnGenerate.setOnClickListener {
             val text = editText.text.toString()
@@ -35,28 +48,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun numberGenerator(text: String, txtResult: TextView) {
-        if (text.isNotEmpty()){
-            val qtd = text.toInt()
-            if (qtd >= 6 && qtd <= 15){
-                val numbers = mutableSetOf<Int>()
-                val random = Random()
-
-                while(true) {
-                    val number = random.nextInt(60)
-                    numbers.add(number + 1)
-
-                    if (numbers.size == qtd) {
-                        break
-                    }
-                }
-
-                txtResult.text = numbers.joinToString ( " - " )
-
-            }else{
-                Toast.makeText(this, "Informe um número enre 6 e 15", Toast.LENGTH_LONG).show()
-            }
-        } else {
+        if (text.isEmpty()){
             Toast.makeText(this, "Informe um número enre 6 e 15", Toast.LENGTH_LONG).show()
+            return
         }
-    }
+
+        val qtd = text.toInt()
+        if (qtd < 6 || qtd > 15){
+            Toast.makeText(this, "Informe um número enre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while(true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString ( " - " )
+        val editor = prefs.edit()
+        editor.putString("result", txtResult.text.toString())
+        editor.apply()
+}
 }

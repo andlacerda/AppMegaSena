80f7922069e6b233e2be591da0907f4635925961
Anderson <andersonlacerdadev@gmail.com>
2025-09-07 11:45:18 -0300

    Implemented a list of the last 5 bets and changed the colors of the app

--- app/src/main/java/com/andlacerda/appmegasena/MainActivity.kt
+++ app/src/main/java/com/andlacerda/appmegasena/MainActivity.kt
@@ -3,8 +3,6 @@ package com.andlacerda.appmegasena
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import java.util.Random
 import android.widget.Button
 import android.widget.EditText
@@ -33,10 +31,20 @@ class MainActivity : AppCompatActivity() {
         val editText: EditText = findViewById(R.id.edit_number)
         val txtResult: TextView = findViewById(R.id.txt_result)
         val btnGenerate: Button = findViewById(R.id.btn_generate)
+        val txt_1: TextView = findViewById(R.id.txt_result_1)
+        val txt_2: TextView = findViewById(R.id.txt_result_2)
+        val txt_3: TextView = findViewById(R.id.txt_result_3)
+        val txt_4: TextView = findViewById(R.id.txt_result_4)
+        val txt_5: TextView = findViewById(R.id.txt_result_5)
 
 
         prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
         val result = prefs.getString("result", null)
+        txt_1.text = prefs.getString("txt1", "")
+        txt_2.text = prefs.getString("txt2", "")
+        txt_3.text = prefs.getString("txt3", "")
+        txt_4.text = prefs.getString("txt4", "")
+        txt_5.text = prefs.getString("txt5", "")
         if (result != null) {
             txtResult.text = "Ultima aposta: $result"
         }
@@ -44,6 +52,29 @@ class MainActivity : AppCompatActivity() {
         btnGenerate.setOnClickListener {
             val text = editText.text.toString()
             numberGenerator(text, txtResult)
+            val allLast= listOf(txt_1, txt_2, txt_3, txt_4, txt_5)
+            val editor = prefs.edit()
+            allLast.forEachIndexed { index, item ->
+                if (item.text == ""){
+                    item.text = prefs.getString("result", "")
+                    editor.putString("txt${index + 1}", item.text.toString())
+                    editor.apply()
+                    return@setOnClickListener
+                }
+            }
+            txt_5.text = txt_4.text
+            txt_4.text = txt_3.text
+            txt_3.text = txt_2.text
+            txt_2.text = txt_1.text
+            txt_1.text = prefs.getString("result", null)
+            editor.apply{
+                putString("txt1", txt_1.text.toString())
+                putString("txt2", txt_2.text.toString())
+                putString("txt3", txt_3.text.toString())
+                putString("txt4", txt_4.text.toString())
+                putString("txt5", txt_5.text.toString())
+                apply()
+            }
         }
     }
 
